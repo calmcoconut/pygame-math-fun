1e83120fbddc5e8a564962e12573537e4f5b4e61
Alejandro <diaz.alejandro772@gmail.com>
2021-07-01 22:45:15 +0900

    background

--- local_settings.py
+++ local_settings.py
@@ -13,5 +13,6 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BACKGROUND = (73, 90, 2)
 
 PATH = os.getcwd()
--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from local_settings import *
 
 mainClock = pygame.time.Clock()
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption(TITLE)
 
-playerRect = pygame.Rect(300, 100, 40, 40)  # left, top, width, height
+backgroundImageMountainsRect = pygame.Rect(0, 0, WINDOWWIDTH, WINDOWHEIGHT // 1.5)
+backgroundImageMountains = pygame.image.load(os.path.join(PATH, 'img', 'country-platform-back.png'))
+mountainsStretchedImage = pygame.transform.scale(backgroundImageMountains, (WINDOWWIDTH, int(WINDOWHEIGHT // 1.5)))
+backgroundImageForests = pygame.image.load(os.path.join(PATH, 'img', 'country-platform-forest.png'))
+backgroundImageForestsRect = pygame.Rect(0, WINDOWHEIGHT - (WINDOWHEIGHT // 1.5), WINDOWWIDTH, backgroundImageForests.get_height())
+forestsStretchedImage = pygame.transform.scale(backgroundImageForests, backgroundImageForestsRect.size)
+
+playerRect = pygame.Rect(10, 400, 40, 40)  # left, top, width, height
 playerImage = pygame.image.load(os.path.join(PATH, 'img', 'player_simple.png'))
 playerStretchedImage = pygame.transform.scale(playerImage, (40, 40))
 
@@ -42,7 +50,7 @@ while True:
             if event.key == K_s:
                 moveDown = False
 
-        windowSurface.fill(BLUE)
+        windowSurface.fill(BACKGROUND)
 
         # move the player
         if moveDown and playerRect.bottom < WINDOWHEIGHT:
@@ -54,6 +62,8 @@ while True:
         if moveRight and playerRect.right < WINDOWWIDTH:
             playerRect.right += PLAYERMOVESPEED
 
+        windowSurface.blit(mountainsStretchedImage, backgroundImageMountainsRect)
+        windowSurface.blit(forestsStretchedImage, backgroundImageForestsRect)
         windowSurface.blit(playerStretchedImage, playerRect)
 
         pygame.display.update()
