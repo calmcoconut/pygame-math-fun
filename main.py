11fe9e7cf7e5a953de55c9e97b37b91e07560fa0
Alejandro <diaz.alejandro772@gmail.com>
2021-07-12 19:52:46 +0900

    change to dictionary data type

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ pygame.display.set_caption(TITLE)
 pygame.font.init()
 font = pygame.font.SysFont(None, 36)
 
-backgroundImageMountainsRect = pygame.Rect(0, 0, WINDOWWIDTH, WINDOWHEIGHT // 1.5)
-backgroundImageMountains = pygame.image.load(os.path.join(PATH, 'img', 'country-platform-back.png'))
-mountainsStretchedImage = pygame.transform.scale(backgroundImageMountains, (WINDOWWIDTH, int(WINDOWHEIGHT // 1.5)))
-backgroundImageForests = pygame.image.load(os.path.join(PATH, 'img', 'country-platform-forest.png'))
-backgroundImageForestsRect = pygame.Rect(0, FORESTSTART, WINDOWWIDTH, backgroundImageForests.get_height())
-forestsStretchedImage = pygame.transform.scale(backgroundImageForests, backgroundImageForestsRect.size)
+# BG 4/10 OF THE SCREEN, FOREGROUND SHOULD BE 4/10 AS WELL
+BG_HEIGHT = int(WINDOWHEIGHT - UIHEIGHT)
+BG_RECT_BACK = pygame.Rect(0, 0, WINDOWWIDTH, int(BG_HEIGHT // 1.5))
+BG_RECT_FORE = pygame.Rect(0, BG_RECT_BACK.centery, WINDOWWIDTH, int(BG_HEIGHT // 1.5))
 # start UI
-UISTART = FORESTSTART + backgroundImageForests.get_height()
-UISIZE = (2 / 3 * WINDOWWIDTH, WINDOWHEIGHT - UISTART)
-UILEFT = (WINDOWWIDTH - UISIZE[0]) / 2
-UIQUESTIONRECT = pygame.Rect(UILEFT + 5, UISTART + 5, UISIZE[0] - 10, UISIZE[1] * 1 / 3)
-UIANSWERRECT = pygame.Rect(UILEFT + 5, UISTART + UISTART * 1 / 6, UISIZE[0] - 10, UISIZE[1] * 1 / 3)
+UI_START = WINDOWHEIGHT - UIHEIGHT
+UI_SIZE = (2 / 3 * WINDOWWIDTH, WINDOWHEIGHT - UI_START)
+UI_LEFT = (WINDOWWIDTH - UI_SIZE[0]) / 2
+UI_QUESTION_RECT = pygame.Rect(UI_LEFT + 5, UI_START + 5, UI_SIZE[0] - 10, UI_SIZE[1] * 1 / 3)
+UI_ANSWER_RECT = pygame.Rect(UI_LEFT + 5, UI_START + UI_START * 1 / 6, UI_SIZE[0] - 10, UI_SIZE[1] * 1 / 3)
+
+IMAGES_DICT = {
+    'bg_mountains': pygame.transform.scale(pygame.image.load(os.path.join(PATH, 'img', 'country-platform-back.png')),
+                                           (WINDOWWIDTH, BG_RECT_BACK.height)),
+    'bg_forests': pygame.transform.scale(pygame.image.load(os.path.join(PATH, 'img', 'country-platform-forest.png')),
+                                         (WINDOWWIDTH, BG_RECT_FORE.height)),
+    'rabbit': pygame.transform.scale(pygame.image.load(os.path.join(PATH, 'img', 'player_simple.png')), (40, 40))
+}
+
+PLAYER_IMAGES = {
+    'rabbit': IMAGES_DICT['rabbit']
+}
+
 
 playerRect = pygame.Rect(10, 400, 40, 40)  # left, top, width, height
 playerImage = pygame.image.load(os.path.join(PATH, 'img', 'player_simple.png'))
@@ -42,13 +53,13 @@ def drawText(text, fontObj, surface, centerCoordsTuple, textColor=None, textBgCo
 
 
 def drawSimpleUI(surface):
-    pygame.draw.rect(surface, STEELBLUE, ((UILEFT, UISTART), UISIZE))
-    pygame.draw.rect(surface, BLUE, UIQUESTIONRECT)
-    pygame.draw.rect(surface, BLACK, UIANSWERRECT)
+    pygame.draw.rect(surface, STEELBLUE, ((UI_LEFT, UI_START), UI_SIZE))
+    pygame.draw.rect(surface, BLUE, UI_QUESTION_RECT)
+    pygame.draw.rect(surface, BLACK, UI_ANSWER_RECT)
 
 
 def drawQuestion(surface, question):
-    drawText(question, font, surface, UIQUESTIONRECT.center)
+    drawText(question, font, surface, UI_QUESTION_RECT.center)
 
 
 def checkInputAnswer(surface, correctAnswer):
@@ -84,7 +95,7 @@ while True:
                 input_active = not input_active
                 oldInput = userInput[:]
                 answer_result = checkInputAnswer(windowSurface, 6)
-                pygame.time.set_timer(USER_ENTERED_ANSW, 600, True) # event, time, once?
+                pygame.time.set_timer(USER_ENTERED_ANSW, 600, True)  # event, time, once?
         if input_active and event.type == KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 userInput = userInput[:-1]
@@ -98,7 +109,7 @@ while True:
             if event.key == K_ESCAPE and input_active:
                 input_active = not input_active
         if event.type == pygame.MOUSEBUTTONUP:
-            if not input_active and UIANSWERRECT.collidepoint(event.pos):
+            if not input_active and UI_ANSWER_RECT.collidepoint(event.pos):
                 input_active = not input_active
         if event.type == USER_ENTERED_ANSW:
             if answer_result:
@@ -110,10 +121,10 @@ while True:
 
         windowSurface.fill(BACKGROUND)
 
-        windowSurface.blit(mountainsStretchedImage, backgroundImageMountainsRect)
-        windowSurface.blit(forestsStretchedImage, backgroundImageForestsRect)
-        windowSurface.blit(playerStretchedImage, playerRect)
+        windowSurface.blit(IMAGES_DICT['bg_mountains'], BG_RECT_BACK)
+        windowSurface.blit(IMAGES_DICT['bg_forests'], BG_RECT_FORE)
+        windowSurface.blit(PLAYER_IMAGES['rabbit'], PLAYER_IMAGES['rabbit'].get_rect())
         drawSimpleUI(windowSurface)
         drawQuestion(windowSurface, "2*3=?")
-        drawText(userInput, font, windowSurface, UIANSWERRECT.center)
+        drawText(userInput, font, windowSurface, UI_ANSWER_RECT.center)
         refreshScreen()
